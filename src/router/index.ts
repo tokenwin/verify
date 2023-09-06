c9c72731e9c8a021eefca30ddc3eef076219a2db
vovachka21 <luchkovladimir97@gmail.com>
2023-09-06 12:37:04 +0300

    Chaged routes for hash routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import CrashView from "../views/CrashView.vue";
 import LimboView from "../views/LimboView.vue";
 import DiceView from "../views/DiceView.vue";
@@ -6,7 +6,7 @@ import MinesView from "../views/MinesView.vue";
 import KenoView from "../views/KenoView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", redirect: "/crash" },
     {
